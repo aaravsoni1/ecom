27168f57308b9dfdd0c63a88cf74218a13ef2ad0
aaravsoni1 <soniaarav596@gmail.com>
2024-08-31 18:12:48 +0530

    created entity

---
+++ src/main/java/com/ecom/ecom/entity/User.java
@@ -0,0 +1,13 @@
+package com.ecom.ecom.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+}
