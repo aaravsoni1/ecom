706dfc85f4ca8a716a7e3f7904655b085beb2053
VrajeshPatel02 <kevalpatel2990@gmail.com>
2024-08-31 18:17:10 +0530

    Entity layer

--- src/main/java/com/ecom/ecom/entity/User.java
+++ src/main/java/com/ecom/ecom/entity/User.java
@@ -1,5 +1,6 @@
 package com.ecom.ecom.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -10,4 +11,10 @@ import lombok.Setter;
 @Entity
 @Table(name = "user")
 public class User {
+    @Column(name = "firstname", nullable = false)
+    private String firstname;
+    private String lastname;
+    private String mobile;
+    private String email;
+
 }
