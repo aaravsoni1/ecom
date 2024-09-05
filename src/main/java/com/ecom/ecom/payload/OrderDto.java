8ce776909dde877773a00de885471af83025c9e0
VrajeshPatel02 <kevalpatel2990@gmail.com>
2024-09-05 22:26:26 +0530

    added OrderDto

---
+++ src/main/java/com/ecom/ecom/payload/OrderDto.java
@@ -0,0 +1,14 @@
+package com.ecom.ecom.payload;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class OrderDto {
+    private Long id;
+    private Integer total_price;
+    private String status;
+    private Date createdAt;
+    private Date updatedAt;
+}
